94116996121f3170db9af700120817d7d28b0d61
CezarV <cezar.vatafu@consensys.net>
2020-04-07 18:16:02 +0300

    adapt network config to Besu local IBFT2 network

--- truffle-config.js
+++ truffle-config.js
@@ -8,14 +8,16 @@ module.exports = {
   networks: {
    development: {
      host: "127.0.0.1",
-     port: 8545,
-      network_id: "*"
+     port: 8590,
+      network_id:44844,
+//      from:0xfe3b557e8fb62b89f4916b721be55ceb828dbd73	
     }
-  //  test: {
-  //    host: "127.0.0.1",
-  //    port: 7545,
-  //    network_id: "*"
-  //  }
+ //   test: {
+ //     host: "127.0.0.1",
+ //     port: 8545,
+ //     network_id: 44844,
+ //     from:0xfe3b557e8fb62b89f4916b721be55ceb828dbd73	
+//    }
   }
-  //
+  
 };
